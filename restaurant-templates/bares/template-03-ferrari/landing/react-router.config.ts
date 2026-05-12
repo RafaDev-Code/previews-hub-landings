c662e4f4bfd82959d02cb2be5691f56cb8a524e0
RafaDev-Code <rafaeldelvalledecatamarca@gmail.com>
2026-05-12 03:39:56 -0300

    fix vercel react router function paths

--- restaurant-templates/bares/template-03-ferrari/landing/react-router.config.ts
+++ restaurant-templates/bares/template-03-ferrari/landing/react-router.config.ts
@@ -3,5 +3,8 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  buildDirectory: "rr",
+  serverBuildFile: "s.js",
+  serverBundles: () => "node",
   presets: [vercelPreset()],
 } satisfies Config;
